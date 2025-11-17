8343fbed3d3bbf30b69ea28b937a387222f59e94
DarioMarchetti <DarioMarchetti@gmail.com>
2025-11-17 18:24:57 +0800

    update entrypoint

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+set -e
+
+initialize() {
+	NODE_DIR=$1
+
+	if [ ! -f "$NODE_DIR/config/genesis.json" ]; then
+		echo "Initializing node directory at $NODE_DIR"
+		cp -r /mova/mainnet-config "$NODE_DIR"
+	fi
+}
+
+
+initialize "/mova/node$ID"
+cd "/mova/node$ID"
+exec supervisord --nodaemon --configuration /etc/supervisor/supervisord.conf
